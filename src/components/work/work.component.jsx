5a4be8011917c0c53e6c44ef1e4c08d12456ffe6
Akbar Maulana <amaulanaa995@gmail.com>
2020-12-29 22:38:54 +0700

    fixed classname on work component

--- src/components/work/work.component.jsx
+++ src/components/work/work.component.jsx
@@ -14,7 +14,7 @@ const Work = () => {
         </Col>
       </WorkRow>
       <Fade top delay={200} duration={1300}>
-        <WorkRow classNam='mr-0 ml-0'>
+        <WorkRow className='mr-0 ml-0'>
           <WorkItem xl={3} md={6} xs={12}>
             <WorkImage src={process.env.PUBLIC_URL + '/images/profile-2.jpg'} alt=""/>
             <WorkTitle>HAI</WorkTitle>
