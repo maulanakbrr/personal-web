5cf832d806057d560c625111d1591ee2c7c87540
Akbar Maulana <amaulanaa995@gmail.com>
2021-03-30 22:28:01 +0700

    updated experience section component to be more mobile responsive

--- src/components/experience/experience.component.jsx
+++ src/components/experience/experience.component.jsx
@@ -3,10 +3,10 @@ import Fade from 'react-reveal/Fade';
 
 import { Col, Image } from 'react-bootstrap';
 
-import { Subtitle, Desc } from '../../App.styles';
+import { Subtitle } from '../../App.styles';
 
 import {
-  ExperienceContainer, ExperienceItem
+  ExperienceContainer, ImageBox, ExperienceItem
 } from './experience.styles';
 
 const Experience = () => {
@@ -16,19 +16,20 @@ const Experience = () => {
       <Subtitle className='experience-title text-justify'>Experience</Subtitle>
 
       <Row>
-        <Col >
-          <div md={6} xs={12}>
-            <Image src={process.env.PUBLIC_URL + '/images/eezy_18.svg'} fluid/>
-          </div>
-        </Col>
         <ExperienceItem md={6} xs={12}>
           <Fade bottom delay={100} duration={1400}>
             <h3>Freelance Web Developer</h3>
             <h6>Self Employed | 2019 - Now</h6>
             <p>I Work on projects provided by client by applying innovative and creative ideas for each project that is done. Standarized all output with new, responsive mobile first-approach and strategy.</p>
-            <p >Project: pilarjuanda, rollogreen</p>
+            <p>Project: pilarjuanda, rollogreen</p>
           </Fade>
         </ExperienceItem>
+        <Col md={6} xs={12}>
+          <ImageBox>
+            <Image src={process.env.PUBLIC_URL + '/images/eezy_18.svg'} fluid/>
+          </ImageBox>
+        </Col>
+        
       </Row>
     </ExperienceContainer>
   );
--- src/components/experience/experience.styles.jsx
+++ src/components/experience/experience.styles.jsx
@@ -4,37 +4,53 @@ import { Container, Col } from 'react-bootstrap';
 export const ExperienceContainer = styled(Container)`
   min-height: 100vh;
   display: flex;
-  /* align-items: center; */
   justify-content: center;
   flex-direction: column;
+  padding-top: 50px;
 
   @media screen and (max-width: 992px){
     padding-top: 50px;
   }
   
   @media screen and (max-width: 767px){
-    padding-top: 90px;
+    padding-top: 50px;
   }
 
   @media screen and (max-width: 576px){
     padding-top: 50px;
+    padding-left: 2rem;
+    padding-right: 2rem;
   }
 
   .experience-title {
     width: 100%;
-    
+  }
+`;
+
+export const ImageBox = styled.div`
+  width: 75%;
+  margin: 0 auto;
+
+  @media screen and (max-width: 991px){
+    margin: 0;
+    margin-left: auto;
+  }
+
+  @media screen and (max-width: 767px){
+    width: 50%;
+    margin: 0 auto;
   }
 `;
 
 export const ExperienceItem = styled(Col)`
   text-align: justify;
-  padding: 0 1rem;
+  padding: 1.2rem 1rem;
 
   @media screen and (max-width: 992px){
     padding: 1rem;
   }
 
   @media screen and (max-width: 767px){
-    text-align: center;
+    padding-bottom: 0;
   }
 `;
